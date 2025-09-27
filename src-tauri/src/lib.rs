f18f7f9a07e80e141e8bbe006c3f912c3cae7930
RusaUB <rustamubajdulloev4@gmail.com>
2025-09-27 17:15:26 +0500

    feat: system-tray-base

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{menu::{Menu, MenuItem}, tray::TrayIconBuilder};
+
 mod cred;
 
 #[tauri::command]
@@ -9,6 +11,35 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let active_agent = MenuItem::with_id(app, "active_agent", "Active Agent", true, None::<&str>)?;
+            let generate_response = MenuItem::with_id(app, "generate_response", "Generate Response", true, None::<&str>)?;
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let menu = Menu::with_items(app, &[&active_agent, &generate_response, &quit])?;
+
+            let _tray = TrayIconBuilder::new()
+                .tooltip("Kairo")
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(move |app, event| {
+                    match event.id.as_ref() {
+                        "quit" => {
+                            app.exit(0);
+                        }
+                        "hide" => {
+                            todo!()
+                        }
+                        "show" => {
+                            todo!()
+                        }
+                        _ => {}
+                    }
+                })
+                .build(app)?;
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet, cred::get_api_key, cred::save_api_key])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
